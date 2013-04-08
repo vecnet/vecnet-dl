4b8d52f75e6c67e583e94ac696d0ae2dec7aa2e3
Banurekha L <blakshmi@nd.edu>
2013-04-08 10:26:02 -0400

    Showing generic file in dashboards

--- app/controllers/role_dashboard_controller.rb
+++ app/controllers/role_dashboard_controller.rb
@@ -90,8 +90,8 @@ class RoleDashboardController < ApplicationController
 
   def exclude_unwanted_models(solr_parameters, user_parameters)
     super
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "-has_model_s:\"info:fedora/afmodel:GenericFile\""
+    #solr_parameters[:fq] ||= []
+    #solr_parameters[:fq] << "-has_model_s:\"info:fedora/afmodel:GenericFile\""
     return solr_parameters
   end
 end
