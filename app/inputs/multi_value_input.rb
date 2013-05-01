fbf1ac7f7ecc7db9c3f650ee080e8c4b0e1d8807
Banurekha L <blakshmi@nd.edu>
2013-05-01 09:58:27 -0400

    Making multi_value input to support textarea

--- app/inputs/multi_value_input.rb
+++ app/inputs/multi_value_input.rb
@@ -4,17 +4,18 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
     input_html_options[:type] ||= 'text'
     input_html_options[:name] ||= "#{object_name}[#{attribute_name}][]"
     input_html_options[:id] ||= "#{attribute_name}"
+    text_type = options.delete(:input_type)
+    puts("Attribute: #{attribute_name}, Collection:#{collection.inspect}, text_type:#{text_type}")
     markup = <<-HTML
 
 
         <ul class="listing">
     HTML
-    puts("Attribute: #{attribute_name}, Collection:#{collection.inspect}, #{collection.size}")
     collection.each do |value|
       unless value.to_s.strip.blank?
         markup << <<-HTML
           <li class="field-wrapper">
-            #{build_text_field(value,'')}
+            #{build_input_field(text_type, value,'')}
           </li>
         HTML
       end
@@ -22,7 +23,7 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
 
     markup << <<-HTML
           <li class="field-wrapper">
-            #{build_text_field('', 'add')}
+            #{build_input_field(text_type, '', 'add')}
           </li>
         </ul>
 
@@ -37,7 +38,7 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
   # It would be _better_ to use @builder construct the element but the proper
   # syntax escapes me.
   def label
-    attribute_label= options[:label] || label_translation || attribute_name.to_s.titleize
+    attribute_label = template.get_label(attribute_name) rescue options[:label] || label_translation || attribute_name.to_s.titleize
     <<-HTML
 
     <label id="#{label_id}" class="string #{label_classes}"> #{attribute_label} </label>
@@ -54,12 +55,30 @@ class MultiValueInput < SimpleForm::Inputs::CollectionInput
     label_html_options[:class].map{|c| c.to_s}.join(' ')
   end
 
+  def build_input_field(type,value,append_to_id)
+    if type.to_s.eql?('textarea')
+      build_text_area(value,append_to_id)
+    else
+      build_text_field(value,append_to_id)
+    end
+  rescue
+    build_text_field(value,append_to_id)
+  end
+
   def build_text_field(value,append_to_id)
     input_html_options[:value] = value
     input_html_options[:id] = append_to_id.blank? ? "#{attribute_name}" : "#{attribute_name}_#{append_to_id}"
     input_html_options[:class] = "#{object_name}_#{attribute_name}"
     input_html_options[:'aria-labelledby'] = label_id
     @builder.text_field(attribute_name, input_html_options)
+  end
+
+  def build_text_area(value,append_to_id)
+    input_html_options[:value] = value
+    input_html_options[:id] = append_to_id.blank? ? "#{attribute_name}" : "#{attribute_name}_#{append_to_id}"
+    input_html_options[:class] = "#{object_name}_#{attribute_name}"
+    input_html_options[:'aria-labelledby'] = label_id
+    @builder.text_area(attribute_name, input_html_options)
   end
 
   protected
