7f2a263adff19258d2828f8c2a18417ccde9a6f1
Banurekha L <blakshmi@nd.edu>
2013-05-01 15:52:48 -0400

    fixing error message within validator

--- app/validators/spatial_validator.rb
+++ app/validators/spatial_validator.rb
@@ -3,13 +3,13 @@ class SpatialValidator < ActiveModel::Validator
   def validate(record)
 
     if nothing_present?(record.latitude,record.longitude)
-      puts "no coordinates and everything good"
+      #puts "no coordinates and everything good"
     elsif both_present?(record.latitude,record.longitude)
-      puts "Both present Lat:#{record.latitude.inspect} long:#{record.longitude.inspect}"
+      #puts "Both present Lat:#{record.latitude.inspect} long:#{record.longitude.inspect}"
       begin
         latitude=record.latitude.is_a?(Array) ? record.latitude : record.latitude.to_s.split()
         longitude=record.longitude.is_a?(Array) ? record.longitude : record.longitude.to_s.split()
-        puts "Lat:#{latitude.reject(&:empty?).length}, long:#{longitude.reject(&:empty?).length}"
+        #puts "Lat:#{latitude.reject(&:empty?).length}, long:#{longitude.reject(&:empty?).length}"
         unless latitude.reject(&:empty?).length.eql?(longitude.reject(&:empty?).length)
           record.errors[:spatials] << "Invalid Spatial Data, both Latitude and Longitude must have value"
         end
@@ -17,9 +17,16 @@ class SpatialValidator < ActiveModel::Validator
         record.errors[:base] << "Invalid Spatial Data"
       end
     else
-      puts "Only one available: Long- #{record.longitude}, Lat- #{record.latitude}"
-      record.errors[:latitude] << "Invalid Spatial Data. latitude is blank" if record.latitude.blank?
-      record.errors[:longitude] << "Invalid Spatial Data. longitude is blank" if record.longitude.blank?
+      #puts "Only one available: Long- #{record.longitude}, Lat- #{record.latitude}"
+      if record.latitude.blank?
+        record.errors[:latitude] << "Invalid Spatial Data. latitude is blank"
+        record.errors[:spatials] << "Invalid Spatial Data. latitude is blank"
+      elsif
+        record.errors[:longitude] << "Invalid Spatial Data. longitude is blank"
+        record.errors[:spatials] << "Invalid Spatial Data. longitude is blank"
+      end
+
+
     end
   end
 
