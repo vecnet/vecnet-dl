861095ec74b21e70a6b4e174e9e7db7d7181dd02
Banurekha L <blakshmi@nd.edu>
2013-05-06 10:34:02 -0400

    Initial version of adding curate gem
    Starting to clean code to fall back to curate gem code... Intial round of cleanup controllers

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ group :default do
   gem 'common_repository_model', git: 'git://github.com/ndlib/common_repository_model'
   #gem 'sufia', git: 'git://github.com/ndlib/sufia.git', branch: 'sufia-for-curate-nd'
   gem 'sufia', path: '../../sufia'
+  gem 'curate', path: '../../curate'
   gem 'solrizer'#, git: 'git://github.com/ndlib/solrizer.git'
   gem 'rsolr', git: 'git://github.com/jeremyf/rsolr.git', branch: 'adding-connection-information-to-error-handling'
   gem 'jettywrapper'
--- app/controllers/classify_concerns_controller.rb
+++ app/controllers/classify_concerns_controller.rb
@@ -1,13 +1,5 @@
-class ClassifyConcernsController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :agreed_to_terms_of_service!
-  layout 'curate_nd/2_column'
-  respond_to :html
-
-  def classify_concern
-    @classify_concern ||= ClassifyConcern.new(params[:classify_concern])
-  end
-  helper_method :classify_concern
+require Curate::Engine.root.join('app/controllers/classify_concerns_controller.rb')
+class ClassifyConcernsController
 
   def new
     if ClassifyConcern.curation_types.size ==1
@@ -16,20 +8,6 @@ class ClassifyConcernsController < ApplicationController
           redirect_to new_polymorphic_path(
                           [:curation_concern, classify_concern.single_curation]
                       )
-        end
-      end
-    else
-      respond_with(classify_concern)
-    end
-  end
-
-  def create
-    if classify_concern.valid?
-      respond_with(classify_concern) do |wants|
-        wants.html do
-          redirect_to new_polymorphic_path(
-            [:curation_concern, classify_concern.curation_concern_class]
-          )
         end
       end
     else
