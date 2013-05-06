68fd2e927f3b534a57c09117245d33d5c66f7f74
Banurekha L <blakshmi@nd.edu>
2013-05-06 12:10:34 -0400

    cleaning up routes after including curate gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,6 @@
 CurateNd::Application.routes.draw do
-  mount_roboto
-
-  Blacklight.add_routes(self)
-  # HydraHead.add_routes(self)
-  # Hydra::BatchEdit.add_routes(self)
 
   devise_for :users
-
-  resources 'dashboard', :only=>:index do
-    collection do
-      get 'page/:page', :action => :index
-      get 'facet/:id',  :action => :facet, :as => :dashboard_facet
-      get 'related/:id',:action => :get_related_file, :as => :related_file
-    end
-  end
-  resources :downloads, only: [:show]
-  resources 'role_dashboard', :only=>:index do
-    collection do
-      get 'page/:page', :action => :index
-      get 'facet/:id',  :action => :facet, :as => :facet
-    end
-  end
 
   resources 'role_dashboard', :only=>:index do
     collection do
@@ -37,29 +17,8 @@ CurateNd::Application.routes.draw do
 
   match "catalog/recent" => "catalog#recent", :as => :catalog_recent
 
-
-  namespace :curation_concern, path: :concern do
-    resources :senior_theses, except: :index
-    resources(
-      :generic_files,
-      only: [:new, :create],
-      path: 'container/:parent_id/generic_files'
-    )
-    resources(
-      :generic_files,
-      only: [:show, :edit, :update, :destroy]
-    )
-    resources :collections
-  end
-
   # Authority vocabulary queries route
   match 'authorities/:model/:term' => 'authorities#query', :via=> :get, :as=>'authority_query'
-
-  resources :terms_of_service_agreements, only: [:new, :create]
-  resources :help_requests, only: [:new, :create]
-  resources :classify_concerns, only: [:new, :create]
-  match "show/:id" => "common_objects#show", via: :get, as: "common_object"
-  match "show/stub/:id" => "common_objects#show_stub_information", via: :get, as: "common_object_stub_information"
   root to: 'catalog#index'
 
 end
