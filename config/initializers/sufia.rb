2e986eec827fcc896a8f60b86cbcbad423f161b8
Banurekha L <blakshmi@nd.edu>
2013-05-09 15:43:10 -0400

    fixing help text, label, resource type and licensing information as per existing prototype

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -17,40 +17,46 @@ Sufia.config do |config|
   config.max_days_between_audits = 7
 
   config.cc_licenses = {
-    'Attribution 3.0 United States' => 'http://creativecommons.org/licenses/by/3.0/us/',
-    'Attribution-ShareAlike 3.0 United States' => 'http://creativecommons.org/licenses/by-sa/3.0/us/',
-    'Attribution-NonCommercial 3.0 United States' => 'http://creativecommons.org/licenses/by-nc/3.0/us/',
-    'Attribution-NoDerivs 3.0 United States' => 'http://creativecommons.org/licenses/by-nd/3.0/us/',
-    'Attribution-NonCommercial-NoDerivs 3.0 United States' => 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/',
-    'Attribution-NonCommercial-ShareAlike 3.0 United States' => 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/',
-    'Public Domain Mark 1.0' => 'http://creativecommons.org/publicdomain/mark/1.0/',
-    'CC0 1.0 Universal' => 'http://creativecommons.org/publicdomain/zero/1.0/',
-    'All rights reserved' => 'All rights reserved'
+      'Attribution 3.0 United States' => 'http://creativecommons.org/licenses/by/3.0/us/',
+      'Attribution-ShareAlike 3.0 United States' => 'http://creativecommons.org/licenses/by-sa/3.0/us/',
+      'Attribution-NonCommercial 3.0 United States' => 'http://creativecommons.org/licenses/by-nc/3.0/us/',
+      'Attribution-NoDerivs 3.0 United States' => 'http://creativecommons.org/licenses/by-nd/3.0/us/',
+      'Attribution-NonCommercial-NoDerivs 3.0 United States' => 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/',
+      'Attribution-NonCommercial-ShareAlike 3.0 United States' => 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/',
+      'Public Domain Mark 1.0' => 'http://creativecommons.org/publicdomain/mark/1.0/',
+      'CC0 1.0 Universal' => 'http://creativecommons.org/publicdomain/zero/1.0/',
+      'All rights reserved' => 'All rights reserved'
   }
 
   config.cc_licenses_reverse = Hash[*config.cc_licenses.to_a.flatten.reverse]
 
   config.resource_types = {
-    "Article" => "Article",
-    "Audio" => "Audio",
-    "Book" => "Book",
-    "Capstone Project" => "Capstone Project",
-    "Conference Proceeding" => "Conference Proceeding",
-    "Dataset" => "Dataset",
-    "Dissertation" => "Dissertation",
-    "Image" => "Image",
-    "Journal" => "Journal",
-    "Map or Cartographic Material" => "Map or Cartographic Material",
-    "Masters Thesis" => "Masters Thesis",
-    "Part of Book" => "Part of Book",
-    "Poster" => "Poster",
-    "Presentation" => "Presentation",
-    "Project" => "Project",
-    "Report" => "Report",
-    "Research Paper" => "Research Paper",
-    "Software or Program Code" => "Software or Program Code",
-    "Video" => "Video",
-    "Other" => "Other",
+      "Article" => "Article",
+      "Audio" => "Audio",
+      "Book" => "Book",
+      "Conference Proceeding" => "Conference Proceeding",
+      "Dataset" => "Dataset",
+      "Dataset: Climate/Weather" => "Dataset: Climate/Weather",
+      "Dataset: Demography" => "Dataset: Demography",
+      "Dataset: Entomological" => "Dataset: Entomological",
+      "Dataset: Epidemiology / Public Health" => "Dataset: Epidemiology / Public Health",
+      "Dataset: GIS" => "Dataset: GIS",
+      "Dataset: Intervention data" => "Dataset: Intervention data",
+      "Dataset: Model Input/Output Data" => "Dataset: Model Input/Output Data",
+      "Dissertation" => "Dissertation",
+      "Image" => "Image",
+      "Journal" => "Journal",
+      "Map or Cartographic Material" => "Map or Cartographic Material",
+      "Poster" => "Poster",
+      "Presentation" => "Presentation",
+      "Project" => "Project",
+      "Report" => "Report",
+      "Research Paper" => "Research Paper",
+      "Software or Program Code" => "Software or Program Code",
+      "Thesis" => "Thesis",
+      "Whitepaper" => "Whitepaper",
+      "Video" => "Video",
+      "Other" => "Other"
   }
 
   config.permission_levels = {
