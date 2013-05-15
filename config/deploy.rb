5c646a7c134a18d293f0c12b3599790537859ea0
Don Brower <dbrower@nd.edu>
2013-05-15 15:21:17 -0400

    Add README and capistrano deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,32 @@
 # NOTE: The SCM command expects to be at the same path on both the local and
 # remote machines. The default git path is: '/shared/git/bin/git'.
 
+set :bundle_roles, [:app]
+set :bundle_flags, "--deployment"
+require 'bundler/capistrano'
+# see http://gembundler.com/v1.3/deploying.html
+# copied from https://github.com/carlhuda/bundler/blob/master/lib/bundler/deployment.rb
+#
+# Install the current Bundler environment. By default, gems will be \
+#  installed to the shared/bundle path. Gems in the development and \
+#  test group will not be installed. The install command is executed \
+#  with the --deployment and --quiet flags. If the bundle cmd cannot \
+#  be found then you can override the bundle_cmd variable to specifiy \
+#  which one it should use. The base path to the app is fetched from \
+#  the :latest_release variable. Set it for custom deploy layouts.
+#
+#  You can override any of these defaults by setting the variables shown below.
+#
+#  N.B. bundle_roles must be defined before you require 'bundler/#{context_name}' \
+#  in your deploy.rb file.
+#
+#    set :bundle_gemfile,  "Gemfile"
+#    set :bundle_dir,      File.join(fetch(:shared_path), 'bundle')
+#    set :bundle_flags,    "--deployment --quiet"
+#    set :bundle_without,  [:development, :test]
+#    set :bundle_cmd,      "bundle" # e.g. "/opt/ruby/bin/bundle"
+#    set :bundle_roles,    #{role_default} # e.g. [:app, :batch]
+
 #############################################################
 #  Settings
 #############################################################
@@ -10,6 +36,7 @@
 default_run_options[:pty] = true
 set :use_sudo, false
 ssh_options[:paranoid] = false
+set :default_shell, '/bin/bash'
 
 #############################################################
 #  SCM
@@ -27,18 +54,28 @@ namespace :env do
   desc "Set command paths"
   task :set_paths do
     set :ruby,      File.join(ruby_bin, 'ruby')
-    set :bundler,   File.join(ruby_bin, 'bundle')
-    set :rake,      "#{bundler} exec #{File.join(shared_path, 'vendor/bundle/bin/rake')}"
+    #set :bundler,   File.join(ruby_bin, 'bundle')
+    #set :bundler,   'bundle'
+    set :rake,      "#{bundle_cmd} exec rake"
   end
 end
+
+# we are using chruby on the deploment machine
+# code from https://github.com/postmodern/chruby/wiki/Capistrano
+set :ruby_version, "1.9.3-p392"
+set :chruby_config, "/etc/profile.d/chruby.sh"
+set :set_ruby_cmd, "source #{chruby_config} && chruby #{ruby_version}"
+set(:bundle_cmd) {
+  "#{set_ruby_cmd} && exec bundle"
+}
 
 #############################################################
 #  Passenger
 #############################################################
 
 desc "Restart Application"
-task :restart_passenger do
-  run "touch #{current_path}/tmp/restart.txt"
+task :restart_unicorn do
+  run "#{current_path}/script/reload-unicorn.sh"
 end
 
 #############################################################
@@ -73,12 +110,12 @@ namespace :deploy do
 
   desc "Start application in Passenger"
   task :start, :roles => :app do
-    restart_passenger
+    restart_unicorn
   end
 
   desc "Restart application in Passenger"
   task :restart, :roles => :app do
-    restart_passenger
+    restart_unicorn
   end
 
   task :stop, :roles => :app do
@@ -98,53 +135,49 @@ namespace :deploy do
     end
   end
 
-  desc "Spool up Passenger spawner to keep user experience speedy"
-  task :kickstart do
-    run "curl -I http://#{domain}"
-  end
-
   desc "Precompile assets"
   task :precompile do
     run "cd #{release_path}; #{rake} RAILS_ENV=#{rails_env} RAILS_GROUPS=assets assets:precompile"
   end
-end
 
-namespace :bundle do
-  desc "Install gems in Gemfile"
-  task :install, :roles => [:app, :work] do
-    switches = ""
-    switches << " --binstubs='#{release_path}/vendor/bundle/bin'"
-    switches << " --shebang '#{ruby}'"
-    switches << " --path=#{release_path}/vendor/bundle"
-    switches << " --gemfile='#{release_path}/Gemfile'"
-    switches << " --deployment"
-    switches << " --without #{without_bundle_environments}"
-    run "#{bundler} install #{switches}"
+  desc "Setup application symlinks for shared assets"
+  task :symlink_setup, :roles => [:app, :web] do
+    shared_directories.each { |link| run "mkdir -p #{shared_path}/#{link}" }
+  end
+
+  desc "Link assets for current deploy to the shared location"
+  task :symlink_update, :roles => [:app, :web] do
+    (shared_directories + shared_files).each do |link|
+      run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
+    end
   end
 end
 
-namespace :worker do
-  task :start, :roles => :work do
-    target_file = "/home/curatend/resque-pool-info"
-    run [
-      "echo \"RESQUE_POOL_ROOT=$(pwd)/current\" > #{target_file}",
-      "echo \"RESQUE_POOL_ENV=#{fetch(:rails_env)}\" >> #{target_file}",
-      "sudo /sbin/service resque-poold restart"
-    ].join(" && ")
-  end
-end
+# this code doesn't work quite right, but it is the way to do it.
+# from http://stackoverflow.com/questions/4648180/keep-unversioned-files-when-deploying-with-capistrano
+# TODO: fix
+#before "deploy:setup" do
+#  symlinks.setup
+#end
+#
+#before "deploy:symlink" do
+#  symlinks.update
+#end
 
-set(:secret_repo_name) {
-  case rails_env
-  when 'staging' then 'secret_staging'
-  when 'pre_production' then 'secret_pprd'
-  when 'production' then 'secret_prod'
-  end
-}
+#namespace :worker do
+#  task :start, :roles => :work do
+#    target_file = "/home/curatend/resque-pool-info"
+#    run [
+#      "echo \"RESQUE_POOL_ROOT=$(pwd)/current\" > #{target_file}",
+#      "echo \"RESQUE_POOL_ENV=#{fetch(:rails_env)}\" >> #{target_file}",
+#      "sudo /sbin/service resque-poold restart"
+#    ].join(" && ")
+#  end
+#end
 
 namespace :und do
   task :update_secrets do
-    run "cd #{release_path} && ./script/update_secrets.sh #{secret_repo_name}"
+    #run "cd #{release_path} && ./script/update_secrets.sh #{secret_repo_name}"
   end
 
   task :write_build_identifier, :roles => :app do
@@ -162,8 +195,8 @@ before 'deploy', 'env:set_paths'
 #  Configuration
 #############################################################
 
-set :application, 'curate_nd'
-set :repository,  "git://github.com/ndlib/curate_nd.git"
+set :application, 'vecnet-dl'
+set :repository,  "git://github.com/banurekha/vecnet.git"
 
 set :build_identifier, Time.now.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -171,40 +204,41 @@ set :build_identifier, Time.now.strftime("%Y-%m-%d %H:%M:%S")
 #  Environments
 #############################################################
 
-desc "Setup for the Pre-Production environment"
-task :pre_production_cluster do
+desc "Setup for the QA environment"
+task :qa do
+  set :shared_directories, %w(log)
+  set :shared_files, %w(config/database.yml config/fedora.yml config/solr.yml config/redis.yml )
   set :symlink_targets do
     [
-      ['/bundle/config','/.bundle/config', '/.bundle'],
+      #['/bundle/config','/.bundle/config', '/.bundle'],
       ['/log','/log','/log'],
-      ['/vendor/bundle','/vendor/bundle','/vendor'],
+      #['/vendor/bundle','/vendor/bundle','/vendor'],
     ]
   end
-  set :branch, "master"
-  set :rails_env,   'pre_production'
-  set :deploy_to,   '/shared/ruby_pprd/data/app_home/curate'
-  set :ruby_bin,    '/shared/ruby_pprd/ruby/1.9.3/bin'
+  set :branch,      'master'
+  set :rails_env,   'qa'
+  set :deploy_to,   '/home/app/vecnet'
+  set :ruby_bin,    '/opt/rubies/1.9.3-p392/bin'
 
-  set :user,        'rbpprd'
-  set :domain,      'curatepprd.library.nd.edu'
+  set :user,        'app'
+  set :domain,      'dl-vecnet-qa.crc.nd.edu'
   set :without_bundle_environments, 'headless development test'
 
   default_environment['PATH'] = "#{ruby_bin}:$PATH"
   server "#{user}@#{domain}", :app, :web, :db, :primary => true
 
-  after 'deploy:update_code', 'und:write_build_identifier', 'und:update_secrets', 'deploy:symlink_shared', 'bundle:install', 'deploy:migrate', 'deploy:precompile'
+  after 'deploy:update_code', 'und:write_build_identifier', 'und:update_secrets', 'deploy:symlink_shared', 'deploy:symlink_update', 'deploy:migrate', 'deploy:precompile'
   after 'deploy', 'deploy:cleanup'
   after 'deploy', 'deploy:restart'
-  after 'deploy', 'deploy:kickstart'
 end
 
 desc "Setup for the Production environment"
 task :production_cluster do
   set :symlink_targets do
     [
-      ['/bundle/config','/.bundle/config', '/.bundle'],
+      #['/bundle/config','/.bundle/config', '/.bundle'],
       ['/log','/log','/log'],
-      ['/vendor/bundle','/vendor/bundle','/vendor'],
+      #['/vendor/bundle','/vendor/bundle','/vendor'],
       #["/config/role_map_#{rails_env}.yml","/config/role_map_#{rails_env}.yml",'/config'],
     ]
   end
@@ -220,10 +254,9 @@ task :production_cluster do
   default_environment['PATH'] = "#{ruby_bin}:$PATH"
   server "#{user}@#{domain}", :app, :web, :db, :primary => true
 
-  after 'deploy:update_code', 'und:write_build_identifier', 'und:update_secrets', 'deploy:symlink_shared', 'bundle:install', 'deploy:migrate', 'deploy:precompile'
+  after 'deploy:update_code', 'und:write_build_identifier', 'und:update_secrets', 'deploy:symlink_shared', 'deploy:migrate', 'deploy:precompile'
   after 'deploy', 'deploy:cleanup'
   after 'deploy', 'deploy:restart'
-  after 'deploy', 'deploy:kickstart'
 end
 
 
@@ -231,9 +264,9 @@ end
 def common_worker_things
   set :symlink_targets do
     [
-      [ '/bundle/config', '/.bundle/config', '/bundle'],
+      #[ '/bundle/config', '/.bundle/config', '/bundle'],
       [ '/log', '/log', '/log'],
-      [ '/vendor/bundle', '/vendor/bundle', '/vendor/bundle'],
+      #[ '/vendor/bundle', '/vendor/bundle', '/vendor/bundle'],
     ]
   end
   set :deploy_to,   '/home/curatend'
@@ -244,16 +277,7 @@ def common_worker_things
   default_environment['PATH'] = "#{ruby_bin}:$PATH"
   server "#{user}@#{domain}", :work
   after 'deploy', 'worker:start'
-  after 'deploy:update_code', 'und:update_secrets', 'deploy:symlink_shared', 'bundle:install'
-end
-
-desc "Setup for the Staging Worker environment"
-task :staging_worker do
-  set :rails_env,   'staging'
-  set :user,        'curatend'
-  set :domain,      'curatestagingw1.library.nd.edu'
-  set :branch, "master"
-  common_worker_things
+  after 'deploy:update_code', 'und:update_secrets', 'deploy:symlink_shared'
 end
 
 desc "Setup for the Preproduction Worker environment"
