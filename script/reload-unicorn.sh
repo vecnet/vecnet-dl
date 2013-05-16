0b76d7f307940b0e1d5ac3e161a6e479209b2488
Don Brower <dbrower@nd.edu>
2013-05-16 14:29:22 -0400

    fix file path for unicorn restart

--- script/reload-unicorn.sh
+++ script/reload-unicorn.sh
@@ -2,16 +2,18 @@
 
 app_root=$(cd $(dirname $0)/.. && pwd)
 
+pid_file="$app_root/tmp/pids/unicorn.pid"
+
 function start_unicorn {
     echo "Starting unicorn"
     $app_root/script/start-server.sh
 }
 
-if [ ! -e unicorn.pid ]; then
+if [ ! -e $pid_file ]; then
 	echo "Cannot find unicorn.pid"
     start_unicorn
 else
-    PID=$(cat $app_root/tmp/pids/unicorn.pid)
+    PID=$(cat $pid_file)
 
     # does process exist?
     if kill -0 $PID; then
