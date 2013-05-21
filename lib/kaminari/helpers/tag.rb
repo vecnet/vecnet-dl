dba367a56bc074bd2c3d90edf4076586a4ad4ecb
Banurekha L <blakshmi@nd.edu>
2013-05-21 15:09:48 -0400

    including patch fix

--- lib/kaminari/helpers/tag.rb
+++ lib/kaminari/helpers/tag.rb
@@ -3,7 +3,7 @@ module Kaminari
   module Helpers
     class Tag
       def page_url_for(page)
-
+        @template.url_for @params.merge(@param_name => (page <= 1 ? nil : page)).symbolize_keys
       end
     end
   end
