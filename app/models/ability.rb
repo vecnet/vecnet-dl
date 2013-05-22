71ad4b69d876814c3464063186081d14772a5575
Banurekha L <blakshmi@nd.edu>
2013-05-22 13:48:55 -0400

    adding superuser admin ability

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include Hydra::Ability
+  def custom_permissions
+    if current_user.admin?
+      can [:edit, :update, :destroy], String
+      can [:edit, :update, :destroy], ActiveFedora::Base
+      can :edit, SolrDocument
+      can :read, String
+      can :read, ActiveFedora::Base
+      can :read, SolrDocument
+    else
+      super
+    end
+  end
+end
