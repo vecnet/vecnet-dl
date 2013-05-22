462934c99c82129c9d300217b728f09474666073
Don Brower <dbrower@nd.edu>
2013-05-22 09:02:04 -0400

    Add usage in comment

--- app/script/batch_to_collection.rb
+++ app/script/batch_to_collection.rb
@@ -2,7 +2,13 @@ class BatchToCollection
   # Transform all Batch objects to Collection objects
   #
   # Used to migrate the Fedora data from scholarsphere data model
-  # to the curate data model
+  # to the curate data model. Usage:
+  #
+  # On rails console type
+  #
+  # a = BatchToCollection.new
+  # a.migrate
+  #
   def migrate
     Batch.all.each do |batch|
       # Thanks to Adam Wead who prointed out this way of
