cc9a3f5a35451954bd804af4cbb8a4e9175acd8f
Don Brower <dbrower@nd.edu>
2013-05-22 07:54:04 -0400

    Add batch to collection data model migration

---
+++ app/script/batch_to_collection.rb
@@ -0,0 +1,14 @@
+class BatchToCollection
+  # Transform all Batch objects to Collection objects
+  #
+  # Used to migrate the Fedora data from scholarsphere data model
+  # to the curate data model
+  def migrate
+    Batch.all.each do |batch|
+      # Thanks to Adam Wead who prointed out this way of
+      # changing fedora object models
+      c = Collection.new(:pid => batch.pid)
+      c.save
+    end
+  end
+end
