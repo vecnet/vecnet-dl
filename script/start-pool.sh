eafd530c9406808f8a90ad340a9f95c833e4be41
Don Brower <dbrower@nd.edu>
2013-06-04 15:16:24 -0400

    Run workers in the correct environment

--- script/start-pool.sh
+++ script/start-pool.sh
@@ -5,8 +5,8 @@ chruby 1.9.3-p392
 
 RAILS_ROOT=/home/app/vecnet/current
 
-RAILS_ENV=dlvecnet bundle exec resque-pool --daemon \
+RAILS_ENV=qa bundle exec resque-pool --daemon \
  --pidfile $RAILS_ROOT/tmp/pids/resque-pool.pid \
  --stdout $RAILS_ROOT/log/resque-pool.stdout.log \
  --stderr $RAILS_ROOT/log/resque-pool.stderr.log \
- --environment dlvecnet
+ --environment qa
