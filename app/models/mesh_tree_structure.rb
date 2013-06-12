5a0f6ddd6f63ca21810541f25e5ee62aa00cf5bb
Don Brower <dbrower@nd.edu>
2013-06-12 16:41:54 -0400

    More tree fun.
    We store an array of the parent terms mapped to strings in a
    database column, with special accessors to handle the mapping between
    Arrays and Strings.

--- app/models/mesh_tree_structure.rb
+++ app/models/mesh_tree_structure.rb
@@ -3,27 +3,33 @@ class MeshTreeStructure < ActiveRecord::Base
   attr_accessible :subject_mesh_term_id, :tree_structure, :eval_tree_path
 
   def self.classify_all_trees
+    MeshTreeStructure.find_each do |mts|
+      mts.classify_tree!
+    end
+  end
+
+  def eval_tree_path
+    read_attribute(:length).split('|')
+  end
+
+  # path should be an array.
+  def eval_tree_path=(path)
+    write_attribute(:eval_tree_path, path.join('|'))
   end
 
   def classify_tree
     tree_levels = initial_segements_of(tree_structure)
-    facet = ''
-    if tree_levels.count < 3
-      facet = lookup_tree(tree_levels.last)
-    else
-      terms = tree_levels[2..-1].map &method(:lookup_tree)
-      facets = []
-      terms_so_far = ''
-      terms.each_with_index do |term, index|
-        terms_so_far << term
-        facets << "#{index}|#{terms_so_far}"
-        terms_so_far << "|"
-      end
-    end
+    tree_levels.map &method(:lookup_tree_term)
+  end
+
+  def classify_tree!
+    eval_tree_path = classify_tree
+    save!
   end
 
   private
-  # Return all of the intial segements of our tree number
+  # Return all of the intial segements of our tree number,
+  # from most general to most specific
   # e.g. 'D03.456.23.789' returns ['D03', 'D03.456', 'D03.456.23', 'D03.456.23.789']
   def initial_segements_of(s)
     result = []
@@ -37,7 +43,7 @@ class MeshTreeStructure < ActiveRecord::Base
 
   # given a tree id, return the main subject term
   # e.g. 'C03.752.530' returns 'Malaria'
-  def lookup_tree(tree_id)
+  def lookup_tree_term(tree_id)
     mts = MeshTreeStructure.find_by_tree_structure(tree_id).subject_mesh_entry
     term = mts.subject_mesh_entry
 
