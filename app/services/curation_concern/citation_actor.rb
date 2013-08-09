451021c974d044185b0b0f3a0397293bfa9a7247
Banurekha <banurekha.l@gmail.com>
2013-08-09 12:25:21 -0400

    makeing citation files as private for now

--- app/services/curation_concern/citation_actor.rb
+++ app/services/curation_concern/citation_actor.rb
@@ -49,7 +49,7 @@ module CurationConcern
       Sufia::GenericFile::Actions.create_metadata(
           citation_file, user, curation_concern.pid
       )
-      citation_file.set_visibility(visibility)
+      citation_file.set_visibility(AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE)
       attach_citation_file(citation_file, user, cf, File.basename(file))
       cf.close
     end
