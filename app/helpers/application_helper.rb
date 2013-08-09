39d5cbd2ff2fbbc431998ccf0e757f0adabe2814
Banurekha <banurekha.l@gmail.com>
2013-08-09 14:05:40 -0400

    converting Related URL to link in show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
       return files_path(solr_document[:noid_s].first)
     end
   end
+
+  def curation_concern_attribute_to_url_html(curation_concern, method_name, label, options = {})
+    markup = ""
+    subject = curation_concern.send(method_name)
+    return markup if !subject.present? && !options[:include_empty]
+    markup << %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
+    [subject].flatten.compact.each do |value|
+      markup << %(<li class="attribute #{method_name}"><a href="#{value}" target="_blank">#{h(value)}</a></li>\n)
+    end
+    markup << %(</ul></td></tr>)
+    markup.html_safe
+  end
+
 end
