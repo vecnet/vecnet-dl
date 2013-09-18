27e09f76a2183589f85efb27349b31c3f8f2d0cc
Banurekha <banurekha.l@gmail.com>
2013-09-18 09:38:40 -0400

    refactoring and bug fixing

--- app/models/geoname_location.rb
+++ app/models/geoname_location.rb
@@ -3,7 +3,7 @@ module GeonameLocation
   extend ActiveSupport::Concern
 
   included do
-    #before_save :format_based_near_from_location
+    before_save :format_based_near_from_location
     attr_accessor :geoname_locations
   end
 
@@ -18,12 +18,12 @@ module GeonameLocation
       input.to_s.gsub(/([=;])/) { "\\#{$1}" }
     end
 
-    def encode
+    def encode_location
       result = []
       hash={}
       hash[:name]=name if name.present?
-      hash[:geoname_id]=geoname_id.present? ? geoname_id : ""
-      Location.encode(hash)
+      hash[:geoname_id]= geoname_id if geoname_id.present?
+      Location.encode_location(hash)
     end
 
     def self.encode_location(input)
@@ -38,11 +38,12 @@ module GeonameLocation
         end
         result.join(";")
       else
-        escape_string(input)
+        #escape_string(input)
+        input.to_s.gsub(/([=;])/) { "\\#{$1}" }
       end
     end
 
-    def self.decode(str)
+    def self.decode_location(str)
       result = {}
       unlabeled_count = 0
       value_list = str.split(/(?<!\\);/)
@@ -62,10 +63,19 @@ module GeonameLocation
     end
 
     def self.parse_location(location_rdf)
-      temp=Location.decode(location_rdf)
-      return Location.new(temp['geoname_id'],temp['name'])
+      temp=Location.decode_location(location_rdf)
+      return Location.new(temp['name'],temp['geoname_id'])
     end
 
+    def formatted_location
+      if name.present? && geoname_id.present?
+        return "#{self.name}|#{self.geoname_id}"
+      elsif name.present?
+        return "#{self.name}|0"
+      elsif geoname_id.present?
+        raise RuntimeError, "invalid location, only id available"
+      end
+    end
 
     def to_s
       if name.present? && geoname_id.present?
@@ -76,22 +86,28 @@ module GeonameLocation
         return "geoname_id:#{geoname_id}"
       end
     end
+
   end
 
   def get_valid_locations
-    self.geoname_locations.reject!{|l| !self.based_near.include?l.split('|').first}
+    locations=self.geoname_locations.blank? ? [] : self.geoname_locations.split(';')
+    unless self.based_near.blank?
+      return locations.reject!{|l| !self.based_near.include?l.split('|').first}
+    else
+      return locations
+    end
   end
 
   def format_based_near_from_location
-    puts "Location array: #{self.geoname_locations}"
+    puts "Location array: #{self.geoname_locations}, locations:#{self.based_near}"
     temp=[]
     locations=get_valid_locations
     locations.each do |location_with_id|
-      test=location_with_id.spilt('|')
-      encoded_location= test.count==2 ? Location.new(test.first, test.last).encode_location : Location.new(test.first, nil).encode_location
+      test=location_with_id.split('|')
+      encoded_location= test.count==2 ? GeonameLocation::Location.new(test.first, test.last).encode_location : GeonameLocation::Location.new(test.first, nil).encode_location
       temp<<encoded_location
     end
+    logger.debug("formatter location: #{temp.inspect}")
     self.based_near=temp
-    return temp
   end
 end
