14e15ca074c27146c8c77f73c871b936c8c5168e
Banurekha <banurekha.l@gmail.com>
2013-09-18 23:27:51 -0400

    adding keyword to metadata update and checking solomon_citation file to process

--- app/services/citation_metadata_ingest_service.rb
+++ app/services/citation_metadata_ingest_service.rb
@@ -3,8 +3,9 @@ class CitationMetadataIngestService
   attr_accessor :metadata_file, :curation_concern, :citations
 
   class CitationData
-    attr_accessor :metadata_file, :curation_concern, :citations
+    attr_accessor :metadata_file, :curation_concern, :metadata_hash
     def initialize(hash)
+      @metadata_hash=hash
       hash.each do |k,v|
         self.instance_variable_set("@#{k}", v)  ## create and initialize an instance variable for this key/value pair
         self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})  ## create the getter that returns the instance variable
@@ -23,9 +24,13 @@ class CitationMetadataIngestService
     end
 
     def subjects
-      puts subject.split("|").map{|t|t.strip}
       return [] if subject.nil?
       return subject.split("|").map{|t|t.strip}
+    end
+
+    def keywords
+      return [] if keyword.nil?
+      return keyword.split("|").delete_if{|name| name.to_s.strip.empty?}.map{|t|t.strip}
     end
 
     def locations
@@ -39,39 +44,50 @@ class CitationMetadataIngestService
     end
 
     def start_time
-      [start_year]
+      [contentyear_start]
     end
 
     def end_time
-      [end_year]
+      [contentyear_end]
     end
 
     def find_citation_by_pid
-      @curation_concern ||= Citation.find(pid)
+      return Citation.find(pid)
     end
 
     def extract_metadata
       metadata=
         {
-          based_near: geoname_locations,   #based_near
+          based_near: geoname_locations,
           subject: subjects,
           start_time:start_time,
-          end_time:end_time
+          end_time:end_time,
+          tag:keywords
         }
       puts "Metadata to update: #{metadata.inspect}"
       return metadata
     end
 
     def update_citation
-      actor.update!
-      logger.info "Ingested Citation with id: @curation_concern.pid}"
-      puts "Ingested Citation with id: @curation_concern.pid}"
-    rescue ActiveFedora::RecordInvalid=>e
-      logger.error "Error occured during creation: #{e.inspect}"
+      begin
+        if curation_concern.nil?
+          logger.error("Could not find citation with pid #{pid.inspect}, failed to update metadata #{self.to_s}")
+        else
+          actor.update!
+          logger.info "Ingested Citation with id: @curation_concern.pid}"
+          puts "Ingested Citation with id: #{@curation_concern.pid}"
+        end
+      rescue ActiveFedora::RecordInvalid=>e
+        logger.error "Error occured during creation: #{e.inspect}"
+      end
     end
 
     def curation_concern
       @curation_concern ||= find_citation_by_pid
+    end
+
+    def to_s
+      return "Metadata to be updated: #{@metadata_hash.inspect}"
     end
 
     include Morphine
@@ -80,7 +96,6 @@ class CitationMetadataIngestService
     end
   end
 
-
   def initialize(csv_file_with_metadata)
     @metadata_file = csv_file_with_metadata
     @citations=[]
@@ -88,20 +103,9 @@ class CitationMetadataIngestService
   end
 
   def ingest_all
-    #citations.each do |c|
-    #  c.ingest_citation
-    #end
-    c=CitationData.new(
-      {
-        :pid => 'vecnet:8623hx72q',
-        :country => "Papua New Guinea|Solomon Islands|Vanuatu|Australia",
-        :place => nil,
-        :start_year => '1991',
-        :end_year => '2009',
-        :subject => "Anopheles farauti 1| Anopheles hinesorum| Anopheles farauti 2| Anopheles irenicus|   Anopheles farauti 7"
-      }
-    )
-    c.update_citation
+    citations.each do |c|
+      c.update_citation
+    end
   end
 
   def process_csv
