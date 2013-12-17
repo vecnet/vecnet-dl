e23fea1aa4988e32315eb30566dbb977c6f1e2ef
Don Brower <dbrower@nd.edu>
2013-12-17 14:25:37 -0500

    Rework log parser logic

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -2,7 +2,7 @@ require 'zlib'
 
 # Given a rails log file, turn it into a sequence of hashes
 # each with the keys:
-# :request :method :ip :user :time :status :duration
+# :path :method :ip :user :date :status :duration
 class LogParser
 
   def scan_gzip(fname, &block)
@@ -20,37 +20,35 @@ class LogParser
   # reads each line from f, extracting request information into a hash
   # hashes are passed to the block as they are read
   def scan(f, &block)
-    start_line = /^Started (?<method>\w+) "(?<path>[^"]+)" for (?<ip>\S+) at (?<date>.*?)$/
-    pubtkt_line = /^Pubtkt:.*?User: (?<user>\w+),/
+    start_line    = /^Started (?<method>\w+) "(?<path>[^"]+)" for (?<ip>\S+) at (?<date>.*?)$/
+    pubtkt_line   = /^Pubtkt:.*?User: (?<user>\w+),/
     complete_line = /^Completed (?<status>\d+).*?in (?<duration>\d+)ms/
-    state = :nothing
+    state = :look_for_start
     info = {}
     f.each_line do |line|
-      m = start_line.match(line)
-      if m
+      case line
+      when start_line
+        m = $~  # save in case $~ is altered in the yield
         yield info if state == :in_body
         info = {method: m[:method],
-                path: m[:path],
-                ip: m[:ip],
-                date: m[:date],
+                path:   m[:path],
+                ip:     m[:ip],
+                date:   m[:date],
         }
         state = :in_body
-        next
-      end
-      next unless state == :in_body
-      m = pubtkt_line.match(line)
-      if m
-        info[:user] = m[:user]
-        next
-      end
-      m = complete_line.match(line)
-      if m
-        info[:status] = m[:status]
-        info[:duration] = m[:duration]
+      when pubtkt_line
+        next unless state == :in_body
+        info[:user] = $~[:user]
+      when complete_line
+        next unless state == :in_body
+        info[:status] = $~[:status]
+        info[:duration] = $~[:duration]
         yield info
-        state = :nothing
+        info = {}
+        state = :look_for_start
       end
     end
+    # in case there is a missing Completed line...
     yield info if state == :in_body
   end
 
