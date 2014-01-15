fac55df886718a8ea3233bdd054609f4e6eaabeb
Banurekha <banurekha.l@gmail.com>
2014-01-15 12:52:16 -0500

    bug with loading schema.. need to uncomment every time I run rspec

--- lib/active_record/active_record_postgres_fk_hack.rb
+++ lib/active_record/active_record_postgres_fk_hack.rb
@@ -2,13 +2,17 @@ if Rails::VERSION::MAJOR < 4
   #Fix fixtures with foreign keys, fixed in Rails4
   class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
     def disable_referential_integrity #:nodoc:
-      if supports_disable_referential_integrity? then
-        execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER" }.join(";"))
-      end
-      yield
-    ensure
-      if supports_disable_referential_integrity? then
-        execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER" }.join(";"))
+      begin
+        if supports_disable_referential_integrity? then
+          execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER" }.join(";"))
+        end
+        #Having trouble in rake rspec on schema load on these two table.. need to rescue them since I could not find solution to fix it
+        #execute(['conversations','notifications'].collect { |name| "DROP TABLE #{quote_table_name(name)} CASCADE" }.join(";"))
+        yield
+      ensure
+        if supports_disable_referential_integrity? then
+          execute(tables.collect { |name| "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER" }.join(";"))
+        end
       end
     end
   end
