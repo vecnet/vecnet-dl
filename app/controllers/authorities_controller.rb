414067d74f39a72e87bddc09a3726a493a215c70
Don Brower <dbrower@nd.edu>
2014-02-12 13:48:31 -0500

    Refactor authorities to use case/when.

--- app/controllers/authorities_controller.rb
+++ app/controllers/authorities_controller.rb
@@ -19,12 +19,15 @@ require File.expand_path('../../models/local_authority', __FILE__)
 class AuthoritiesController < ApplicationController
   def query
     s = params.fetch("q", "")
-    if (params[:term]=="location")
+    case params[:term]
+    when "location"
       hits = GeoNamesResource.find_location(s)
-    elsif (params[:term]=="species")
+    when "species"
       hits = LocalAuthority.entries_by_species(params[:term], s) #rescue []
-    else
+    when "subject"
       hits = LocalAuthority.entries_by_subject_mesh_term(params[:model], params[:term], s) #rescue []
+    else
+      hits = []
     end
     render :json=>hits
   end
