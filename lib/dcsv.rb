25a26f5ca77ed3249d85e8e19c12a9987e3d7db6
Don Brower <dbrower@nd.edu>
2014-03-11 10:52:45 -0400

    Add dublin core tuple decoder (dcsv)

---
+++ lib/dcsv.rb
@@ -0,0 +1,43 @@
+class Dcsv
+  # Takes an input string in the DCSV format
+  # (see http://dublincore.org/documents/dcmi-dcsv/ )
+  # and returns a hash.
+  #
+  # TODO: This does not handle the '.' scope operator at all
+  def self.decode(input_string)
+    result = {}
+    unlabeled_count = 0
+    value_list = input_string.split(/(?<!\\);/)
+    value_list.each do |value|
+      value.gsub!(/\\;/,';') # unescape ;
+      label, rest = value.split(/(?<!\\)=/,2)
+      label.gsub!(/\\=/, '=')
+      rest.gsub!(/\\=/, '=') if rest
+      if rest
+        result[label] = rest
+      else
+        result[unlabeled_count] = label
+        unlabeled_count += 1
+      end
+    end
+    result
+  end
+
+  # takes an input hash or string and converts it
+  # into the DCSV format. Returns a string.
+  def self.encode(input)
+    if input.is_a?(Hash)
+      result = []
+      input.each_pair do |k,v|
+        if k.is_a?(Integer)
+          result << encode(v)
+        else
+          result << "#{encode(k)}=#{encode(v)}"
+        end
+      end
+      result.join("; ")
+    else
+      input.to_s.gsub(/([=;])/) { "\\#{$1}" }
+    end
+  end
+end
---
+++ spec/lib/dcsv_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'dcsv'
+
+describe Dcsv do
+  describe "decode" do
+    it "handles escaped characters" do
+      output = Dcsv.decode("a;b\\;c=hello;d\\=nothing=nothing;e;")
+      output.should == {  0 => "a",
+                          "b;c" => "hello",
+                          "d=nothing" => "nothing",
+                          1 => "e"
+                       }
+    end
+    it "handles values with escaped =" do
+      output = Dcsv.decode("a;b\\=c;d;")
+      output.should == {  0 => "a",
+                          1 => "b=c",
+                          2 => "d"
+                       }
+    end
+  end
+
+  describe "encode" do
+    it "escapes strings correctly" do
+      output = Dcsv.encode("=ab;")
+      output.should == "\\=ab\\;"
+    end
+    it "converts hashes recursively" do
+      output = Dcsv.encode({"a" => "b", "c=d" => ";"})
+      output.should == "a=b; c\\=d=\\;"
+    end
+    it "notices array indexing and encodes that" do
+      output = Dcsv.encode({0 => "a=b", 1 => ";", 2 => "asdf", "123" => "+345"})
+      output.should == "a\\=b; \\;; asdf; 123=+345"
+    end
+  end
+end
