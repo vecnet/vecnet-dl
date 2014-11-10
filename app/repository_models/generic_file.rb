1bc2f7dd74679f80093f065fdae0f45fc6dddf37
Don Brower <dbrower@nd.edu>
2014-11-10 14:42:13 -0500

    Remove dead code in generic file

--- app/repository_models/generic_file.rb
+++ app/repository_models/generic_file.rb
@@ -90,9 +90,8 @@ class GenericFile
   end
 
   def locations
-    locations=self.based_near
-    new_locations=locations.map{|loc| refactor_location(loc) }
-    new_locations
+    locations = self.based_near
+    locations.map{ |loc| refactor_location(loc) }
   end
 
   def refactor_location(location)
@@ -108,65 +107,4 @@ class GenericFile
       generic_file.save!
     end
   end
-=begin
-  def get_hierarchy_on_location
-    unless self.based_near.blank?
-      geoname_id_hash= LocationHierarchyServices.get_geoname_ids(self.based_near)
-      location_trees=[]
-      location_tree_to_solrize=[]
-      geoname_id_hash.each do |location,geoname_id|
-        hierarchy= GeonameHierarchy.find_by_geoname_id(geoname_id)
-        hierarchy_with_earth=''
-        if hierarchy && hierarchy.hierarchy_tree_name.present?
-          hierarchy_with_earth= hierarchy.hierarchy_tree_name.gsub(';',':')
-        else
-          tree_id, tree_name = LocationHierarchyServices.find_hierarchy(geoname_id)
-          hierarchy_with_earth=tree_name.gsub(';',':')
-        end
-        hierarchy_without_earth=hierarchy_with_earth.gsub('Earth:','')
-        location_tree_to_solrize<<hierarchy_without_earth
-      end
-      location_trees<<location_tree_to_solrize.collect{|tree| LocationHierarchyServices.get_solr_hierarchy_from_tree(tree)}.flatten
-      return location_trees.flatten
-    end
-    return nil
-  end
-
-  def get_formated_date_created
-    return nil if self.date_created.blank?
-    return @pub_date_sort.to_time.utc.iso8601 unless @pub_date_sort.nil?
-    pub_date=self.date_created.first
-    if self.date_created.size>1
-      logger.error "#{self.pid} has more than one pub date, #{self.date_created.inspect}, but will only use #{pub_date} for sorting"
-    end
-    pub_date_replace=pub_date.gsub(/-|\/|,|\s/, '.')
-    @pub_date_sort=pub_date_replace.split('.').size> 1? Chronic.parse(pub_date) : Date.strptime(pub_date,'%Y')
-    return @pub_date_sort.to_time.utc.iso8601 unless @pub_date_sort.blank?
-  end
-
-  def get_subject_parents
-    subjects=self.subject
-    all_trees_arr=[]
-    subjects.each do |sub|
-      mesh_subject= SubjectMeshEntry.find_by_term(sub)
-      if mesh_subject
-        all_trees_arr<<mesh_subject.mesh_tree_structures.collect{|tree| tree.get_solr_hierarchy_from_tree}.flatten
-      end
-    end
-    return all_trees_arr.uniq
-  end
-
-  def get_hierarchical_faceting_on_subject
-    subjects=self.subject
-    all_trees=[]
-    subjects.each do |sub|
-      mesh_subject= SubjectMeshEntry.find_by_term(sub)
-      if mesh_subject
-        all_trees<<mesh_subject.mesh_tree_structures.collect{|tree| tree.get_solr_hierarchy_from_tree}.flatten
-      end
-    end
-    return all_trees.flatten
-  end
-=end
 end
-
