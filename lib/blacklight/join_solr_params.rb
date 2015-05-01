5d6634d38371ba83a27e00fc697185bef9091353
Don Brower <dbrower@nd.edu>
2015-05-01 10:28:52 -0400

    Quote full text search strings
    But it is still broken for queries which have unmatched quotes. Fixing
    that will require going into blacklight.

--- lib/blacklight/join_solr_params.rb
+++ lib/blacklight/join_solr_params.rb
@@ -9,13 +9,17 @@ module JoinSolrParams
     solr_search_params_logic << :add_join_query_to_solr
   end
 
+  def escape_quotes(s)
+    s.gsub('"', '\\\\"').gsub("'", "\\\\'")
+  end
+
   #If include full_text on citation is selected that need to join on citation file solr document
   #to match on parse withing fulltext
   def add_join_query_to_solr(solr_parameters, user_parameters = params)
     if user_parameters[:include_full_text] && user_parameters[:citation]
       solr_parameters[:q] ||= []
       join_caluse = solr_parameters[:q].empty? ? '' : ' OR '
-      solr_parameters[:q] << join_caluse + "_query_:\"{!join from=parent_id_s to=id}all_text_unstem_search:#{user_parameters[:citation]}\""
+      solr_parameters[:q] << join_caluse + "_query_:\"{!join from=parent_id_s to=id}all_text_unstem_search:#{escape_quotes(user_parameters[:citation])}\""
     end
     return solr_parameters
   end
