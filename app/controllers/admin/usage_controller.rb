2598b1ce73aa89a2146f5a2100c120dc8ec23dc8
Don Brower <dbrower@nd.edu>
2015-05-13 10:44:53 -0400

    Reporting form date picking.
    Make it pretty.

--- app/controllers/admin/usage_controller.rb
+++ app/controllers/admin/usage_controller.rb
@@ -5,8 +5,8 @@ module Admin
 
     def index
       # get result and repackage it as we intend to display it
-      @start = date_from_hash(params[:start])
-      @end = date_from_hash(params[:end])
+      @start = date_from_param(params[:start])
+      @end = date_from_param(params[:end])
       result = UsageEvent.resource_reporting(@start, @end)
                .group_by { |r| r["resource_type"] }
       @total = {"view" => 0, "download" => 0}
@@ -25,12 +25,10 @@ module Admin
       @events = UsageEvent.order("event_time DESC").limit(200)
     end
 
-    def date_from_hash(hash, default=nil)
-      return default if hash.nil?
-      return nil if hash[:year] == ""
-      hash[:month] = 1 if hash[:month] == ""
-      hash[:day] = 1 if hash[:day] == ""
-      Date.new(hash[:year].to_i, hash[:month].to_i, hash[:day].to_i)
+    def date_from_param(hash)
+      return nil if hash.nil?
+      return nil unless hash =~ /\A(\d{4})-(\d{1,2})-(\d{1,2})\Z/
+      Date.new($1.to_i, $2.to_i, $3.to_i)
     end
 
   end
